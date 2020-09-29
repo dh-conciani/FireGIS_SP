cd0474544d8a4ad23ec5f6d79c0b1630af608b55
Dhemerson Conciani <dhemerson_conciani@yahoo.com.br>
2020-09-29 17:02:27 -0300

    Update _buildVM.sh

--- web-application/_buildVM.sh
+++ web-application/_buildVM.sh
@@ -1,4 +1,6 @@
 ## e2-small	2vCPUs (25%) + burst 100% (2-3min)	2GB RAM us-west Oregon $12.23 month
+## Ubuntu 16.04 LTS
+
 ## set user privileges
 sudo passwd
 ******** # set
@@ -10,51 +12,68 @@ passwd dh_conciani
 gpasswd -a dh_conciani sudo
 su dh_conciani
 
-## install nginx server
-sudo apt-get update
-sudo apt-get -y install nginx
-
-## create 3GB swap (ubuntu 16.04 lts)
+## create swap partition
 cd /
 sudo dd if=/dev/zero of=swapfile bs=1M count=3000
 sudo mkswap swapfile
 sudo swapon swapfile
 sudo nano etc/fstab
-/swapfile none swap sw 0 0 ^X
+/swapfile none swap sw 0 0
 cat /proc/meminfo
 
-## install r 
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list'
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
+## install nginx server
 sudo apt-get update
-sudo apt-get -y install r-base
+sudo apt-get -y install nginx
 
-## update r base
-sudo su
-echo "deb http://www.stats.bris.ac.uk/R/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-apt-get update
-apt-get upgrade
-
-## update ubuntu libraries
-sudo apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
+## install system dependencies
 sudo apt-get update
+## libraries
 sudo apt-get install libudunits2-dev
-
-## install GDAL
+## GDAL
 sudo add-apt-repository ppa:ubuntugis/ppa
 sudo apt-get update
 sudo apt-get install gdal-bin
-
-## build python GDAL
 sudo apt-get install libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL==<GDAL VERSION FROM OGRINFO>
+
+## install r-base
+sudo bash -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran40/" >> /etc/apt/sources.list' && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && sudo apt update
+sudo apt install r-base
 
 ## install r packages
-sudo su - -c "R -e \"install.packages(c('devtools','rmarkdown', 'quantmod'), repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages(c('codetools'))\""
+sudo su - -c "R -e \"install.packages(c('shinyjs'))\""
+sudo su - -c "R -e \"install.packages(c('lattice'))\""
+sudo su - -c "R -e \"install.packages(c('sp'))\""
+sudo su - -c "R -e \"install.packages(c('leaflet'))\""
+sudo su - -c "R -e \"install.packages(c('leaflet.extras'))\""
+sudo su - -c "R -e \"install.packages(c('stringi'))\""
+sudo su - -c "R -e \"install.packages(c('sf'))\""
+sudo su - -c "R -e \"install.packages(c('rgdal'))\""
+
+
+
+
+
+
+## update ubuntu libraries
+#sudo apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
+#sudo apt-get update
+#sudo apt-get install libudunits2-dev
+
+## install GDAL
+#sudo add-apt-repository ppa:ubuntugis/ppa
+#sudo apt-get update
+#sudo apt-get install gdal-bin
+
+## build python GDAL
+#sudo apt-get install libgdal-dev
+#export CPLUS_INCLUDE_PATH=/usr/include/gdal
+#export C_INCLUDE_PATH=/usr/include/gdal
+#pip install GDAL==<GDAL VERSION FROM OGRINFO>
+
+## install r packages
 sudo su - -c "R -e \"install.packages(c('shinyjs','leaflet', 'leaflet.extras', 'rgdal', 'stringi', 'sf', 'raster', 'htmlwidgets', 'shiniydashboard', 'ggplot2', 'bsplus'), repos='http://cran.rstudio.com/')\""
 
 ## install r studio server
