66a3b3d6bd1508cbe73597409ab7106e8e94188b
Dhemerson Conciani <dhemerson_conciani@yahoo.com.br>
2020-09-29 09:50:59 -0300

    Update _buildVM.sh

--- web-application/_buildVM.sh
+++ web-application/_buildVM.sh
@@ -41,4 +41,11 @@ sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.1.456-amd64.deb
 sudo gdebi rstudio-server-1.1.456-amd64.deb
 
+## install shiny server
+sudo su - -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
+sudo apt-get install gdebi-core
+sudo wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.9.923-amd64.deb
+sudo gdebi shiny-server-1.5.9.923-amd64.deb
+
+## server started at 3838 port
 
