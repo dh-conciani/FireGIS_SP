50af1303afe4a9bf81f0b510d804ff5342f1603a
Dhemerson Conciani <dhemerson_conciani@yahoo.com.br>
2020-09-29 10:51:41 -0300

    Update _buildVM.sh

--- web-application/_buildVM.sh
+++ web-application/_buildVM.sh
@@ -35,6 +35,7 @@ sudo apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
 ## install r packages
 sudo su - -c "R -e \"install.packages(c('devtools','rmarkdown', 'quantmod'), repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages(c('shinyjs','leaflet', 'leaflet.extras', 'rgdal', 'stringi', 'sf', 'raster', 'htmlwidgets', 'shiniydashboard', 'ggplot2', 'bsplus'), repos='http://cran.rstudio.com/')\""
+devtools::install_github('rspatial/raster')
 
 ## install r studio server
 sudo apt-get install gdebi-core
